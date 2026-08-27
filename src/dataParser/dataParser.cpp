ea949a158525aa4cae4a90b341084172617f9b1c
Bujdoso Balint <bujdosobalint8@gmail.com>
2026-08-27 18:51:23 +0200

    a

--- src/dataParser/dataParser.cpp
+++ src/dataParser/dataParser.cpp
@@ -2,6 +2,7 @@
 #include<vector>
 #include<fstream>
 #include "orderbook/order.h"
+#include <stack>
 class DataParser{
 public:
 		  static std::vector<Order> readData(const std::string& filename){
@@ -20,8 +21,29 @@ public:
 		  }
 };
 
+std::unique_ptr<Order> createOrder(std::string& in){
+	 
+}
+
 int main(){
 	 DataParser d;
 	 d.readData("test.csv");
 	 return 0;
 }
+/*
+{
+  "id": "e2a85d9f-07a5-4f94-8d5f-789dc3deb097",
+  "method": "order.place",
+  "params": {
+    "symbol": "BTCUSDT",
+    "side": "BUY",
+    "type": "LIMIT",
+    "price": "0.1",
+    "quantity": "10",
+    "timeInForce": "GTC",
+    "timestamp": 1655716096498,
+    "apiKey": "T59MTDLWlpRW16JVeZ2Nju5A5C98WkMm8CSzWC4oqynUlTm1zXOxyauT8LmwXEv9",
+    "signature": "5942ad337e6779f2f4c62cd1c26dba71c91514400a24990a3e7f5edec9323f90"
+  }
+}
+*/
